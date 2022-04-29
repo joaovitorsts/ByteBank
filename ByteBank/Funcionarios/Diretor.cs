83492de3b8ef22265310023f9b04b2e5c7a1d6a1
João Vitor dos Santos <jsantos@isolutions.com.br>
2022-04-29 10:53:17 -0300

    Sintaxe

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor: Funcionario
+    public class Diretor : Funcionario
     {
         public override double GetBonificacao()
         {
