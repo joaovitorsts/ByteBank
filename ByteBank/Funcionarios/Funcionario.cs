eb502a77aacd7a4798690c0365a1ccef0e138e01
João Vitor dos Santos <jsantos@isolutions.com.br>
2022-04-29 10:41:47 -0300

    Herança diretor

--- ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -8,5 +8,13 @@ namespace ByteBank.Funcionarios
 {
     public class Funcionario
     {
+        public string Nome { get; set; }
+        public string CPF{ get; set; }
+        public double Salario { get; set; }
+
+        public virtual double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
     }
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ByteBank;
+using ByteBank.Funcionarios;
+
+GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+Funcionario carlos = new Funcionario();
+carlos.Nome = "Carlos";
+carlos.CPF = "546.879.157-20";
+carlos.Salario = 2000;
+
+gerenciador.Registrar(carlos);
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta";
+roberta.CPF = "454.658.148-3";
+roberta.Salario = 5000;
+
+gerenciador.Registrar(roberta);
+
+Console.WriteLine(carlos.Nome);
+Console.WriteLine(carlos.GetBonificacao());
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
+
+Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+Console.ReadLine();
